74b5a4f46178ea4afd24a40975533fb5a4013092
sebas98tian <juansebastianbernal2@gmail.com>
2025-03-07 21:12:01 -0500

    primer cambio

---
+++ index.js
@@ -0,0 +1,21 @@
+let nextSlide=document.querySelectorAll(".nextSlide");
+let contador=1;
+let slider=document.querySelector(".slider");
+for(let i=0; i<nextSlide.length; i++){
+    nextSlide[i].addEventListener("click",()=>{
+        slider.style.transition=`1s`;
+        slider.style.transform=`translateX(${-contador*100}%)`;
+        contador++;
+        console.log(contador);
+        if(contador==4){
+            setTimeout(() => {
+                for(let j=0; j<nextSlide.length; j++){
+                    nextSlide[j].style.display="none";
+                    document.documentElement.style.overflowY = "visible";
+                    document.body.style.overflowY = "visible";
+    
+                }
+            }, 1000);
+        }
+    })
+}
